c677c36c9c8d3f64f868edea8bb3a819e15c937d
Alex Holden <63453554+alexanderholden@users.noreply.github.com>
2026-04-19 01:50:49 +0000

    u

--- sap/sap.vhd
+++ sap/sap.vhd
@@ -2,22 +2,24 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
--- c_p = when active increment on falling edge
--- e_p = when active put program register onto bus
--- nclr = active low clr
--- nclk = active low clk
+-- CP = when active increment on falling edge
+-- EP = when active put program register onto bus
+-- nCLR = active low clr
+-- nCLK = active low clk
 
-entity programcounter is -- WORKING
+entity program_counter is -- WORKING
   port (
-    C_P        : in std_logic;
+    CP         : in std_logic;
     nCLK       : in std_logic;
     nCLR       : in std_logic;
-    E_P        : in std_logic; -- will be used on top level implementation
+    EP         : in std_logic;
+    LP         : in std_logic;
+    pc_bus_in  : in std_logic_vector(3 downto 0);
     pc_bus_out : out std_logic_vector(7 downto 0)
   );
 end;
 
-architecture programcounter_rtl of programcounter is
+architecture program_counter_rtl of program_counter is
   signal programcounter_reg : unsigned(3 downto 0) := (others => '0');
 begin
   process (nCLK)
@@ -25,30 +27,32 @@ begin
     if nCLR = '0' then
       programcounter_reg <= (others => '0');
     elsif falling_edge(nCLK) then
-      if C_P = '1' then
+      if CP = '1' then
         programcounter_reg <= programcounter_reg + 1;
+      elsif LP = '1' then
+        programcounter_reg <= UNSIGNED(pc_bus_in);
       end if;
     end if;
   end process;
-  pc_bus_out <= "0000" & std_logic_vector(programcounter_reg) when E_P = '1' else
+  pc_bus_out <= "0000" & std_logic_vector(programcounter_reg) when EP = '1' else
     (others => 'Z');
 end architecture;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
---L_A = active low loading into accumulator
---e_a = active high loading from accumulator onto bus
--- CLK = active high clock
+--LA = active low loading into accumulator
+--EA = active high loading from accumulator onto bus
+--CLK = active high clock
 
 entity accumulator is --WORKING
   port (
-    L_A         : in std_logic;
-    CLK         : in std_logic;
-    E_A         : in std_logic;
-    acc_bus_in  : in std_logic_vector(7 downto 0);
-    acc_bus_out : out std_logic_vector(7 downto 0);
-    addsub_in   : out std_logic_vector(7 downto 0)
+    LA            : in std_logic;
+    CLK           : in std_logic;
+    EA            : in std_logic;
+    acc_bus_in    : in std_logic_vector(7 downto 0);
+    acc_bus_out   : out std_logic_vector(7 downto 0);
+    add_sub_input : out std_logic_vector(7 downto 0)
 
   );
 end;
@@ -58,16 +62,16 @@ begin
   process (CLK)
   begin
     if rising_edge(CLK) then
-      if L_A = '1' then
+      if LA = '1' then
         accumulator_reg <= acc_bus_in;
       end if;
     end if;
   end process;
 
-  acc_bus_out <= accumulator_reg when E_A = '1' else
+  acc_bus_out <= accumulator_reg when EA = '1' else
     (others => 'Z');
 
-  addsub_in <= accumulator_reg;
+  add_sub_input <= accumulator_reg;
 end architecture;
 
 library IEEE;
@@ -79,164 +83,148 @@ use IEEE.numeric_std.all;
 -- b_bus_in = accepts data from bus
 -- b_addsum_in = outputs to addsum (hello hi)
 
-entity b_reg is --WORKING
+entity b_register is --WORKING
   port (
-    L_B         : in std_logic;
-    CLK         : in std_logic;
-    b_bus_in    : in std_logic_vector(7 downto 0);
-    b_addsum_in : out std_logic_vector(7 downto 0)
-
+    LB           : in std_logic;
+    CLK          : in std_logic;
+    b_bus_in     : in std_logic_vector(7 downto 0);
+    b_add_sum_in : out std_logic_vector(7 downto 0)
   );
 end;
 
-architecture b_rtl of b_reg is
+architecture b_register_rtl of b_register is
   signal b_reg : std_logic_vector(7 downto 0) := (others => '0');
 begin
   process (CLK)
   begin
     if rising_edge(CLK) then
-      if L_B = '1' then
+      if LB = '1' then
         b_reg <= b_bus_in;
       end if;
     end if;
   end process;
-
-  b_addsum_in <= b_reg;
+  b_add_sum_in <= b_reg;
 end architecture;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
--- fucking logisim made me call this shit hello hi, its the add sub
 -- A = accumulator input
 -- B = b register input
--- s_u = subtract/add mode swap, 0 = add, 1 = sub
--- e_u = output to bus
+-- SU = subtract/add mode swap, 0 = add, 1 = sub
+-- EU = output to bus
 
-entity hello_hi is
+entity add_sub is
   port (
-    A                : in std_logic_vector(7 downto 0);
-    B                : in std_logic_vector(7 downto 0);
-    S_U              : in std_logic;
-    E_U              : in std_logic;
-    hello_hi_bus_out : out std_logic_vector(7 downto 0)
+    A               : in std_logic_vector(7 downto 0);
+    B               : in std_logic_vector(7 downto 0);
+    SU              : in std_logic;
+    EU              : in std_logic;
+    ADD_SUB_bus_out : out std_logic_vector(7 downto 0)
   );
 end;
 
-architecture hello_hi_rtl of hello_hi is
-
+architecture add_sub_rtl of add_sub is
   signal result : std_logic_vector(7 downto 0);
 begin
-
-  process (A, B, S_U)
+  process (A, B, SU)
   begin
-    if S_U = '0' then
+    if SU = '0' then
       result <= std_logic_vector(unsigned(A) + unsigned(B));
     else
       result <= std_logic_vector(unsigned(A) - unsigned(B));
     end if;
   end process;
-
-  hello_hi_bus_out <= result when E_U = '1' else
+  add_sub_bus_out <= result when EU = '1' else
     (others => 'Z');
-
 end;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
---mar_addr = input from mar for which address to access
--- nCE = active low chip enable (output)
--- data_out_bus = output to bus
-
 entity ram16x8 is
   port (
-    mar_addr     : in std_logic_vector (3 downto 0);
-    CE           : in std_logic;
-    data_out_bus : out std_logic_vector(7 downto 0);
-    WE           : in std_logic;
-    data_input   : in std_logic_vector (7 downto 0);
-    addr_input   : in std_logic_vector (3 downto 0);
-    clk          : in std_logic;
-    nCLR         : in std_logic
+    mar_addr    : in std_logic_vector(3 downto 0);
+    CE          : in std_logic; -- active high
+    ram_bus_out : out std_logic_vector(7 downto 0)
   );
 end;
 
 architecture rtl_ram of ram16x8 is
   type ram_type is array (0 to 15) of std_logic_vector(7 downto 0);
   signal ram : ram_type := (
-  0 => "00000101", -- LDA 5
-  1 => "00010110", -- ADD 6
-  2 => "11100000", -- output
-  5 => "00000101",
-  6 => "00000101",
-  others => (others => '0')
+  0  => "00001001", -- LDA @9
+  1  => "00011100", -- ADD @12
+  2  => "11101111", -- OUT
+  3  => "01100001", -- JMP @1
+  4  => "01100001", -- HLT (unused)
+  5  => "00000000",
+  6  => "00000000",
+  7  => "00000000",
+  8  => "00000000",
+  9  => "00000001",
+  10 => "00000001",
+  11 => "00000001",
+  12 => "00000001",
+  13 => "00000000",
+  14 => "00000000",
+  15 => "00000000"
   );
 begin
-  data_out_bus <= ram(to_integer(unsigned(mar_addr))) when CE = '1' else
+  ram_bus_out <= ram(to_integer(unsigned(mar_addr))) when CE = '1' else
     (others => 'Z');
-  process (clk)
-  begin
-    if rising_edge(clk) then
-      if WE = '1' then
-        ram(to_integer(unsigned(addr_input))) <= data_input;
-      elsif nCLR = '0' then
-        ram <= (others => (others => '0'));
-      end if;
-    end if;
-  end process;
 end architecture;
-
-  library IEEE;
-  use IEEE.std_logic_1164.all;
-  use IEEE.numeric_std.all;
-
-  -- nLM = active low allowing input from bus into mar
-  -- bus_in = bus input
-  -- mar_addr_out = address output to ram
-
-  entity mar is
-    port (
-      LM           : in std_logic;
-      CLK          : in std_logic;
-      bus_in       : in std_logic_vector(3 downto 0);
-      mar_addr_out : out std_logic_vector(3 downto 0)
-    );
-  end;
-
-  architecture mar_rtl of mar is
-    signal mar_reg : std_logic_vector(3 downto 0) := (others => '0');
-  begin
-    process (CLK)
-    begin
-      if rising_edge(CLK) then
-        if LM = '1' then
-          mar_reg <= bus_in; -- only lower 4 bits
-        end if;
-      end if;
-    end process;
-
-    mar_addr_out <= mar_reg;
-  end architecture;
 
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity instructionregister is
+-- nLM = active low allowing input from bus into mar
+-- bus_in = bus input
+-- mar_addr_out = address output to ram
+
+entity mar is
   port (
-    L_I           : in std_logic;
+    LM               : in std_logic;
+    CLK              : in std_logic;
+    bus_in           : in std_logic_vector(3 downto 0);
+    mar_addr_bus_out : out std_logic_vector(3 downto 0)
+  );
+end;
+
+architecture mar_rtl of mar is
+  signal mar_reg : std_logic_vector(3 downto 0) := (others => '0');
+begin
+  process (CLK)
+  begin
+    if rising_edge(CLK) then
+      if LM = '1' then
+        mar_reg <= bus_in; -- only lower 4 bits
+      end if;
+    end if;
+  end process;
+
+  mar_addr_bus_out <= mar_reg;
+end architecture;
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity instruction_register is
+  port (
+    LI            : in std_logic;
     CLK           : in std_logic;
     CLR           : in std_logic;
-    E_I           : in std_logic;
+    EI            : in std_logic;
     ir_bus_in     : in std_logic_vector (7 downto 0);
     ir_addr_out   : out std_logic_vector (3 downto 0);
     ir_opcode_out : out std_logic_vector (3 downto 0)
   );
 end;
 
-architecture instructionregister_rtl of instructionregister is
+architecture instruction_register_rtl of instruction_register is
   signal ir_reg : std_logic_vector (7 downto 0) := (others => '0');
 begin
   process (clk, CLR)
@@ -244,13 +232,13 @@ begin
     if CLR = '0' then
       ir_reg <= (others => '0');
     elsif rising_edge(clk) then
-      if L_I = '1' then
+      if LI = '1' then
         ir_reg <= ir_bus_in;
       end if;
     end if;
   end process;
   ir_opcode_out <= ir_reg(7 downto 4);
-  ir_addr_out   <= ir_reg (3 downto 0) when E_I = '1' else
+  ir_addr_out   <= ir_reg (3 downto 0) when EI = '1' else
     (others => 'Z');
 end architecture;
 
@@ -260,10 +248,10 @@ use IEEE.numeric_std.all;
 
 entity outregister is
   port (
-    L_O         : in std_logic;
-    CLK         : in std_logic;
-    out_bus_in  : in std_logic_vector (7 downto 0);
-    out_reg_out : out std_logic_vector (7 downto 0)
+    LO              : in std_logic;
+    CLK             : in std_logic;
+    out_bus_in      : in std_logic_vector (7 downto 0);
+    out_reg_bus_out : out std_logic_vector (7 downto 0)
   );
 end;
 
@@ -273,149 +261,148 @@ begin
   process (clk)
   begin
     if rising_edge(clk) then
-      if L_O = '1' then
+      if LO = '1' then
         out_reg <= out_bus_in;
       end if;
     end if;
   end process;
-  out_reg_out <= out_reg;
+  out_reg_bus_out <= out_reg;
 end architecture;
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.STD_LOGIC_1164.all;
+use IEEE.STD_LOGIC_ARITH.all;
+use IEEE.STD_LOGIC_UNSIGNED.all;
 
 entity Controller is
-    Port ( clk : in  STD_LOGIC;
-			  clr : in  STD_LOGIC;
-			  inst_in : in STD_LOGIC_VECTOR (3 downto 0);
-			  Cp : out STD_LOGIC;
-			  Ep : out STD_LOGIC;
-			  Lm : out STD_LOGIC;
-			  CE : out STD_LOGIC;
-			  Li : out STD_LOGIC;
-			  Ei : out STD_LOGIC;
-			  La : out STD_LOGIC;
-			  Ea : out STD_LOGIC;
-			  Su : out STD_LOGIC;
-			  Eu : out STD_LOGIC;
-			  Lb : out STD_LOGIC;
-			  Lo : out STD_LOGIC;
-			  HLT : out STD_LOGIC);
+  port (
+    clk     : in std_logic;
+    clr     : in std_logic;
+    inst_in : in std_logic_vector (3 downto 0);
+    Cp      : out std_logic;
+    Ep      : out std_logic;
+    Lm      : out std_logic;
+    CE      : out std_logic;
+    Li      : out std_logic;
+    Ei      : out std_logic;
+    La      : out std_logic;
+    Ea      : out std_logic;
+    Su      : out std_logic;
+    Eu      : out std_logic;
+    Lb      : out std_logic;
+    Lo      : out std_logic;
+    Lp      : out std_logic;
+    HLT     : out std_logic);
 end Controller;
 
 architecture Behavioral of Controller is
 
-type state is (idle, t0, t1, t2, t3, t4, t5);
-signal pr_state, nx_state : state := t0;
-signal control_signal : STD_LOGIC_VECTOR (11 downto 0) := (others => '0');
-signal HLT_sig : STD_LOGIC := '1';
+  type state is (idle, t0, t1, t2, t3, t4, t5);
+  signal pr_state, nx_state : state                          := t0;
+  signal control_signal     : std_logic_vector (12 downto 0) := (others => '0');
+  signal HLT_sig            : std_logic                      := '1';
 
 begin
+  process (clk, clr)
+  begin
+    if clr = '0' then
+      pr_state <= idle;
+    elsif rising_edge(clk) then
+      pr_state <= nx_state;
+    end if;
+  end process;
+  process (pr_state)
+  begin
+    case pr_state is
+      when idle =>
+        --Do nothing
+        control_signal <= "0000000000000";
+        HLT_sig        <= '1';
+        nx_state       <= t0;
 
--- This process if for updating current state.
-process(clk, clr)
-begin
-	if clr = '0' then
-		pr_state <= idle;
-	elsif rising_edge(clk) then
-		pr_state <= nx_state;
-	end if;
-end process;
+      when t0 =>
+        --Enable PC (Ep = 1), Load into MAR (Lm = 1)
+        control_signal <= "0011000000000";
+        nx_state       <= t1;
 
+      when t1 =>
+        --Increment PC (Cp = 1)
+        control_signal <= "0100000000000";
+        nx_state       <= t2;
 
--- This process does the actual transition logic and operations.
-process(pr_state)
-begin
-	case pr_state is
-		when idle =>
-			--Do nothing
-			control_signal <= "000000000000";
-			HLT_sig <= '1';
-			nx_state <= t0;
+      when t2 =>
+        --Enable memory (CE = 1), Load into IR (Li = 1)
+        control_signal <= "0000110000000";
+        nx_state       <= t3;
 
-		when t0 =>
-			--Enable PC (Ep = 1), Load into MAR (Lm = 1)
-			control_signal <= "011000000000";
-			nx_state <= t1;
-		
-		when t1 =>
-			--Increment PC (Cp = 1)
-			control_signal <= "100000000000";
-			nx_state <= t2;
-		
-		when t2 =>
-			--Enable memory (CE = 1), Load into IR (Li = 1)
-			control_signal <= "000110000000";
-			nx_state <= t3;
-			
-		when t3 =>
-			--OUT
-			if inst_in = "1110" then
-				--Enable AC, Load into OUT
-				control_signal <= "000000010001";
-			--HALT
-			elsif inst_in = "1111" then
-				control_signal <= "000000000000";
-				HLT_sig <= '0';
-			--Other instructions
-			else
-				control_signal <= "001001000000";
-			end if;
-			nx_state <= t4;
-		
-		when t4 =>
-			--LDA
-			if inst_in = "0000" then
-				--Enable memory, Load into AC
-				control_signal <= "000100100000";
-			--ADD
-			elsif inst_in = "0001" then
-				--Enable memory, Load into B
-				control_signal <= "000100000010";
-			--SUB
-			elsif inst_in = "0010" then
-				--Enable memory, Load into B
-				control_signal <= "000100000010";
-			--Other instructions
-			else
-				control_signal <= "000000000000";
-			end if;
-			nx_state <= t5;
-			
-		when t5 =>
-			--ADD
-			if inst_in = "0001" then
-				--Enable ALU, Load into AC, Su = 0 for ADD
-				control_signal <= "000000100100";
-			--SUB
-			elsif inst_in = "0010" then
-				--Enable ALU, Load into AC, Su = 1 for SUB
-				control_signal <= "000000101100";
-			--Other instructions
-			else
-				control_signal <= "000000000000";
-			end if;
-			nx_state <= t0;
-			
-	end case;
-end process;
+      when t3 =>
+        --OUT
+        if inst_in = "1110" then
+          --Enable AC, Load into OUT
+          control_signal <= "0000000010001";
+          --HALT
+        elsif inst_in = "1111" then
+          control_signal <= "0000000000000";
+          HLT_sig        <= '0';
+          --Other instructions
+        elsif inst_in = "0110" then
+          control_signal <= "1000001000000";
+        else
+          control_signal <= "0001001000000";
+        end if;
+        nx_state <= t4;
 
---Move control signal to output
+      when t4 =>
+        --LDA
+        if inst_in = "0000" then
+          --Enable memory, Load into AC
+          control_signal <= "0000100100000";
+          --ADD
+        elsif inst_in = "0001" then
+          --Enable memory, Load into B
+          control_signal <= "0000100000010";
+          --SUB
+        elsif inst_in = "0010" then
+          --Enable memory, Load into B
+          control_signal <= "0000100000010";
+          --Other instructions
+        else
+          control_signal <= "0000000000000";
+        end if;
+        nx_state <= t5;
 
-Cp <= control_signal(11);
-Ep <= control_signal(10);
-Lm <= control_signal(9);
-CE <= control_signal(8);
-Li <= control_signal(7);
-Ei <= control_signal(6);
-La <= control_signal(5);
-Ea <= control_signal(4);
-Su <= control_signal(3);
-Eu <= control_signal(2);
-Lb <= control_signal(1);
-Lo <= control_signal(0);
-HLT <= HLT_sig;
+      when t5 =>
+        --ADD
+        if inst_in = "0001" then
+          --Enable ALU, Load into AC, Su = 0 for ADD
+          control_signal <= "0000000100100";
+          --SUB
+        elsif inst_in = "0010" then
+          --Enable ALU, Load into AC, Su = 1 for SUB
+          control_signal <= "0000000101100";
+          --Other instructions
+        else
+          control_signal <= "0000000000000";
+        end if;
+        nx_state <= t0;
+
+    end case;
+  end process;
+
+  --Move control signal to output
+  Lp  <= control_signal(12);
+  Cp  <= control_signal(11);
+  Ep  <= control_signal(10);
+  Lm  <= control_signal(9);
+  CE  <= control_signal(8);
+  Li  <= control_signal(7);
+  Ei  <= control_signal(6);
+  La  <= control_signal(5);
+  Ea  <= control_signal(4);
+  Su  <= control_signal(3);
+  Eu  <= control_signal(2);
+  Lb  <= control_signal(1);
+  Lo  <= control_signal(0);
+  HLT <= HLT_sig;
 
 end Behavioral;
